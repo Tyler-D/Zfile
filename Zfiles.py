12beccb19549cda9187443384e64a2ef8fb0262d
Tyler <tylerzy@foxmail.com>
2015-10-23 23:31:19 -0700

    making DiskHandler

--- Zfiles.py
+++ Zfiles.py
@@ -19,27 +19,20 @@ define("mysql_password", default="123", help="db password")
 
 TEMPLATE_PATH = os.path.join(os.path.dirname(__file__), "templates")  
 STATIC_PATH = os.path.join(os.path.dirname(__file__), "static") 
-
+UPLOADED_FILES = os.path.join(os.path.dirname(__file__),"files")
 class BaseHandler(tornado.web.RequestHandler):
-    def get_curret_user(self):
+    def get_current_user(self):
         return self.get_secure_cookie("user")
     def check_user(self):
         if not self.current_user:
             self.redirect("/login")
             return 
-    def req_params(self):
-        return tornado.input()
     def _print(self, page_name, base =None):
         pass
 
 class MyRequestHandler(tornado.web.RequestHandler):
     def check_xsrf_cookie(self):
         pass
-class HomeHandler(BaseHandler):
-    def get(self):
-        self.check_user()
-        self.render("templates/index.html")
-
 def user_auth(name, pw=None):
     db = torndb.Connection(
         host = options.mysql_host,
@@ -48,16 +41,17 @@ def user_auth(name, pw=None):
         password = options.mysql_password
         )
     user = db.get("select * from user where username=%s", name)
+    print(user)
     if not pw:
         if user == None:
                 return False
         return True
     else:
-        if name==user[1]:
-            if pw==user[2]:
-                return True
-            else:
-                return False
+        print(pw, user['username']);
+        if pw==user['password']:
+            return True
+        else:
+            return False
         return False
 #Account Action
 class RegisterHandler(BaseHandler):
@@ -77,19 +71,34 @@ class RegisterHandler(BaseHandler):
         else:
             self.redirect("/register")
 
-class LoginHandler(BaseHandler):
+class HomeHandler(BaseHandler):
     def get(self):
-       self.render() 
+       cookie = self.get_current_user() 
+       db = self.application.db
+       files = db.query("select * from files order by last_modify limit 10")
+       self.render("index.html", cookieName = cookie, recent = files) 
     def post(self):
         name = self.get_argument("username")
         pw = self.get_argument("password")
         res = user_auth(name,pw)
-
+        print(res)
         if res :
             self.set_secure_cookie("user", name)
             self.redirect("/disk")
         else:
-            self.redirect("/login")
+            self.redirect("/")
+class LogoutHandler(BaseHandler):
+    def get(self):
+        self.set_secure_cookie("user", 'None')
+        self.redirect("/")
+class DiskHandler():
+    def get(self):
+        self.check_user()
+        db = self.application.db
+        files = db.query("select * from files order by last_modify limit 10") 
+        self.render("disk.html", files = files)
+    def post(self):
+        
 '''        
 #File Action
 class FileHandler(BaseHandler):
@@ -115,7 +124,7 @@ class Application(tornado.web.Application):
         handlers =[
             (r"/", HomeHandler),
             (r"/register", RegisterHandler),
-            (r"/login", LoginHandler),
+            (r"/logout", LogoutHandler),
         ]
         settings = dict(
             template_path = TEMPLATE_PATH,
